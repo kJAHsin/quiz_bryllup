9259fa9cc3d4e476734b249c3a1210554fdd307c
Kenneth Jahnsen Collins <106404582+kJAHsin@users.noreply.github.com>
2024-07-27 22:08:13 +0200

    finished score update

--- src/lib/updateScore.js
+++ src/lib/updateScore.js
@@ -1,14 +1,34 @@
-import { Client, Databases } from "node-appwrite";
+import { Client, Databases } from "appwrite";
+import { ID } from "appwrite";
+
+const API_ENDPOINT = "https://cloud.appwrite.io/v1";
+const PROJECT_ID = "66a34bdf0001b827496a";
+const DB_ID = "quiz-questions";
+const COLLECTION_ID = "66a41e950014df0e4f65";
 
 const client = new Client()
-	.setEndpoint("https://cloud.appwrite.io/v1") // Your API Endpoint
-	.setProject("5df5acd0d48c2") // Your project ID
-	.setKey("919c2d18fb5d4...a2ae413da83346ad2"); // Your secret API key
+	.setEndpoint(API_ENDPOINT)
+	.setProject(PROJECT_ID)
+	.setSession("");
 
 const databases = new Databases(client);
 
-const result = await databases.updateCollection(
-	"<DATABASE_ID>", // databaseId
-	"<COLLECTION_ID>", // collectionId
-	"<COLLECTION_NAME>" // name
-);
+export async function updateScoreForUser({ userID, score }) {
+	try {
+		const DOCUMENT_ID = ID.unique();
+
+		const updatedDocument = await databases.createDocument(
+			DB_ID,
+			COLLECTION_ID,
+			DOCUMENT_ID,
+			{ user: userID, score }
+		);
+
+		if (updatedDocument) {
+			console.log(updatedDocument);
+			return updatedDocument;
+		}
+	} catch (err) {
+		console.error(err);
+	}
+}
