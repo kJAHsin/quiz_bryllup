57eac4f8dbf389fd2271ee364263893448d897dd
Kenneth Jahnsen Collins <106404582+kJAHsin@users.noreply.github.com>
2024-08-01 15:30:17 +0200

    logged out return of function

--- src/App.jsx
+++ src/App.jsx
@@ -86,7 +86,7 @@ function App() {
 		createAccount({ email, password, userID });
 	}
 
-	listDocs();
+	console.log(listDocs());
 
 	const switchSummaryText = () => {
 		return correctCount === questionArr.length
